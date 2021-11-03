d79dcd0dd1909237a6ded26c5797f8ab7c4da49b
Jeff McHale <rhithiktheawesome@gmail.com>
2021-11-02 21:37:58 -0400

    nresponsive menu implemented.

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const year = date.getFullYear();
 
 document.getElementById("currentYear").innerHTML = year;
 
-// navigation
+// navigation toggle
+const menuButton = document.getElementsByClassName("menu-btn")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
+
+menuButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
