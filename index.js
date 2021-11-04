a3e286d69e935766f939e95ea394313417dfbab3
the_rhithik <jeffmchale08@gmail.com>
2021-11-03 23:08:42 -0400

    refactored services section into cards and use flexbox more efficiently. added fixed navigation. removed some whitespace from CSS. renamed some classes to make more sense.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ const navLinks = document.getElementsByClassName("nav-links")[0];
 menuButton.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+// sticky navigation
+const nav = document.querySelector(".navbar")
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  console.log(topOfNav)
+  if(window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav)
