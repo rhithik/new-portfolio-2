a0c426bc843aa50d2a498d5535de232fb2dcc166
the_rhithik <jeffmchale08@gmail.com>
2021-11-04 23:32:33 -0400

    commenting out all form validation in js for now. will work on it after my brain has rested

--- index.js
+++ index.js
@@ -13,16 +13,43 @@ menuButton.addEventListener("click", () => {
 });
 
 // sticky navigation
-const nav = document.querySelector(".navbar")
+const nav = document.querySelector(".navbar");
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-  console.log(topOfNav)
-  if(window.scrollY >= topOfNav) {
+  console.log(topOfNav);
+  if (window.scrollY >= topOfNav) {
     document.body.classList.add("fixed-nav");
   } else {
     document.body.classList.remove("fixed-nav");
   }
 }
 
-window.addEventListener("scroll", fixNav)
+window.addEventListener("scroll", fixNav);
+
+// form validation
+// const nameInput = document.getElementById("name");
+// const emailInput = document.getElementById("email");
+
+// nameInput.addEventListener("input", () => {
+//   nameInput.setCustomValidity("");
+//   nameInput.checkValidity();
+// });
+
+// nameInput.addEventListener("invalid", () => {
+//   nameInput.setCustomValidity("Please enter your name.");
+// });
+
+// emailInput.addEventListener("input", () => {
+//   emailInput.setCustomValidity("");
+//   emailInput.checkValidity();
+// });
+
+// emailInput.addEventListener("invalid", () => {
+//   emailInput.setCustomValidity("Please enter a valid email address.");
+// });
+
+// function validateEmail(email) {
+//   var regex = /\S+@\S+\.\S+/;
+//   return regex.test(email);
+// }
